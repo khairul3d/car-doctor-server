6bccded15e304d5550349260041d3117186fa1c6
khairul3d <mdkhairulislam.3735@gmail.com>
2024-05-04 12:46:36 +0600

    cookie secure added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require ('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,13 +26,72 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const serviceCollection = client.db('carDoctor').collection('servises')
+    const chackOutCollection = client.db('carDoctor').collection('chackout')
+
+    // jwt
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+       console.log(user);
+       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn:'1h'})
+       res
+       .cookie('token', token,{
+        httpOnly: true, 
+        recure: false, 
+        sameSite: 'none'
+       })
+      .send({success: true})
+    })
+
+    app.get('/servises', async(req, res)=> {
+        const cursor = serviceCollection.find()
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.get('/servises/:id', async (req, res) => {
+        const id = req.params.id;
+        const quary = { _id: new ObjectId(id)}
+
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { title:1, price:1, service_id:1, img:1 },
+          };
+
+        const result = await serviceCollection.findOne(quary, options)
+        res.send(result)
+    })
+
+    // chackingOut 
+
+    app.get('/chackout', async(req, res)=> {
+      console.log(req.query.email);
+      let query = {}
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await chackOutCollection.find(query).toArray();
+      res.send(result)
+
+  })
+
+    app.post('/chackout', async(req, res)=> {
+        const chackout = req.body;
+        console.log(chackout);
+        const result = await chackOutCollection.insertOne(chackout)
+        res.send(result)
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
